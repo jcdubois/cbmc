0ce7f786358a15dfbbe066d1064eb6b51a2c5731
Daniel Kroening <dkr@amazon.com>
2022-04-23 23:06:26 +0100

    fx tests

--- regression/cprover/cstrings/cstring_is_memory_safe2.c
+++ regression/cprover/cstrings/cstring_is_memory_safe2.c
@@ -2,7 +2,7 @@ int main()
 {
   const char *p;
   __CPROVER_size_t i;
-  __CPROVER_assume(__CPROVER_is_cstring(p+i));
+  __CPROVER_assume(__CPROVER_is_cstring(p + i));
   // this is now safe
   p[i];
 
--- regression/cprover/cstrings/iterate_over_string3.c
+++ regression/cprover/cstrings/iterate_over_string3.c
@@ -1,24 +1,28 @@
 #define size_t __CPROVER_size_t
 #define false 0
 
-_Bool compare(const void *const array, const size_t array_len, const char *const c_str)
+_Bool compare(
+  const void *const array,
+  const size_t array_len,
+  const char *const c_str)
 {
-//  const unsigned char *array_bytes = array;
+  //  const unsigned char *array_bytes = array;
   const unsigned char *str_bytes = (const unsigned char *)c_str;
 
   // jointly iterate over string and array
-  for (size_t i = 0; i < array_len; ++i)
+  for(size_t i = 0; i < array_len; ++i)
     __CPROVER_loop_invariant(__CPROVER_is_cstring(str_bytes + i))
-  {
-    unsigned char s = str_bytes[i];
-    if (s == '\0') {
+    {
+      unsigned char s = str_bytes[i];
+      if(s == '\0')
+      {
         return false;
-    }
+      }
 
-//    if (array_bytes[i] != s) {
-//        return false;
-//    }
-  }
+      //    if (array_bytes[i] != s) {
+      //        return false;
+      //    }
+    }
 
   //return str_bytes[array_len] == '\0';
 }
